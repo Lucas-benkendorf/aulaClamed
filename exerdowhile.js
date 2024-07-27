fc563bb1a682d071bbc716ceccf455a702dc586d
Lucas Benkendorf <benklucass@gmail.com>
2024-07-26 22:10:10 -0300

    alterar

--- exerdowhile.js
+++
@@ -1,8 +0,0 @@
-var senha = 1234
-
-do { 
-    var senhaDigitada = prompt("Digita sua senha:")
-    if (senhaDigitada == senha){
-        alert("Acesso permitido")
-    }
-} while (senhaDigitada != senha)
